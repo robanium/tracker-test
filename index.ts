08213dfccf9366b8220b16e856510f39c4628279
Steven Glukhov <steven.glukhov@gmail.com>
2022-10-13 20:13:58 +0300

    Add express.js skeleton to server

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import config from "./config.json";
 import runClient from "./client";
+import runServer from "./server";
 
 // @ts-ignore
 runClient(config.client);
+// @ts-ignore
+runServer(config.server);
---
+++ server/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import routes from "./routes";
+
+export default function (config: { port: number; address: string }) {
+  const app = express();
+
+  app.use(routes);
+
+  app.listen(config.port, config.address, () => {
+    console.log(
+      "Backend Server Started at http://" +
+        config.address +
+        ":" +
+        config.port +
+        "/"
+    );
+  });
+}
