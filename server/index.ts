9939c326765f6eef9e24afa309a1c0b90fb9d2a0
Steven Glukhov <steven.glukhov@gmail.com>
2022-10-13 20:34:20 +0300

    Add CORS Origin header to server response

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
 import routes from "./routes";
+import cors from "cors";
 
-export default function (config: { port: number; address: string }) {
+export default function (config: any) {
   const app = express();
 
+  app.disable("etag");
+  app.disable("x-powered-by");
+  app.use(cors(config.cors || {}));
   app.use(routes);
 
   app.listen(config.port, config.address, () => {
